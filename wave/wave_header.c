467faed10870248de833bb13228b7a5657571e54
Lucio Mitsuru Seki <lucioseki@gmail.com>
2012-10-12 01:58:03 -0300

    Simplifiquei o printf dos campos long equivalentes a char[4].

--- wave/wave_header.c
+++ wave/wave_header.c
@@ -2,17 +2,10 @@
 #include "wave_header.h"
 
 void show_header(HeaderType *header){
-	long label;
-
-	label = header->ChunkID;
-	printf("ChunkID: %c%c%c%c\n", label, label >> 8, label >> 16, label >> 24);
+	printf("ChunkID: %.4s\n", &(header->ChunkID));
 	printf("ChunkSize: %ld\n", header->ChunkSize);
-
-	label = header->Format;
-	printf("Format: %c%c%c%c\n", label, label >> 8, label >> 16, label >> 24);
-
-	label = header->Subchunk1ID;
-	printf("Subchunk1ID: %c%c%c%c\n", label, label >> 8, label >> 16, label >> 24);
+	printf("Format: %.4s\n", &(header->Format));
+	printf("Subchunk1ID: %.4s\n", &(header->Subchunk1ID));
 	printf("Subchunk1Size: %ld\n", header->Subchunk1Size);
 	printf("AudioFormat: %hu\n", header->AudioFormat);
 	printf("NumChannels: %hu\n", header->NumChannels);
@@ -20,8 +13,6 @@ void show_header(HeaderType *header){
 	printf("ByteRate: %ld\n", header->ByteRate);
 	printf("BlockAlign: %hu\n", header->BlockAlign);
 	printf("BitsPerSample: %hu\n", header->BitsPerSample);
-
-	label = header->Subchunk2ID;
-	printf("Subchunk2ID: %c%c%c%c\n", label, label >> 8, label >> 16, label >> 24);
+	printf("Subchunk2ID: %.4s\n", &(header->Subchunk2ID));
 	printf("Subchunk2Size: %ld\n", header->Subchunk2Size);
 }
