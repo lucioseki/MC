4e4ecaac7bd334ba8e05a7546616bb8cb92c6c61
Lucio Mitsuru Seki <lucioseki@gmail.com>
2012-10-29 11:18:51 -0200

    O programa copia o input.wav para output.wav.

--- wave/main.c
+++ wave/main.c
@@ -1,20 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "wave_header.h"
 
 int main(int argc, char** argv){
 	HeaderType header;
+	char* data;
 
-	if(argc != 2){
-		printf("Usage: main <filename.wav>\n");
+	if(argc < 2 || argc > 3){
+		printf("Usage: main <input.wav> [output.wav]\n");
 		return 1;
 	}
 
 	FILE *fp = fopen(argv[1], "rb");
+	if(!fp){
+		perror(argv[1]);
+		return 1;
+	}
 	fread(&header, sizeof(header), 1, fp);
-	fclose(fp);
-
 	show_header(&header);
+
+	data = malloc(header.Subchunk2Size);
+	fread(data, header.Subchunk2Size, 1, fp);
+	fclose(fp);
+	
+	if(argc > 2){
+		fp = fopen(argv[2], "wb");
+		if(!fp){
+			perror(argv[1]);
+			return 1;
+		}
+
+		fwrite(&header, sizeof(header), 1, fp);
+		fwrite(data, header.Subchunk2Size, 1, fp);
+		fclose(fp);
+	}
 
 	return 0;
 }
